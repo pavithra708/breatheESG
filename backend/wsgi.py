b31f54cbff6944502d47305b3ed958c1987c3274
pavithra <pavithrarevankar@gmail.com>
2026-05-28 20:52:27 +0530

    Fix Django module imports by running from backend directory

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,6 +5,6 @@ WSGI config for Breathe ESG project.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
